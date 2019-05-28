7a4834d99de4faad03b9c2f88b5183f4d32ea448
Demian Holderegger <demian.holderegger@gmail.com>
2019-05-28 10:34:44 +0200

    add bootstrap-vue modal example

--- demo/src/views/Home.vue
+++ demo/src/views/Home.vue
@@ -4,15 +4,28 @@
     <img alt="Vue logo" src="../assets/logo.png" class="logo">
     <br><br>
     <i class="fa fa-beer"></i>
+    <div>
+      <h3>Modal Demo</h3>
+      <b-button @click="$refs.modal1.show()">Launch demo modal</b-button>
+      <b-modal id="modal-1" title="BootstrapVue" ref="modal1">
+        <p class="my-4">Hello from modal!</p>
+      </b-modal>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import {IAppState} from "@/store/modules/App"; // @ is an alias to /src
+import {IAppState} from "@/store/modules/App";
+import {BModal} from "bootstrap-vue";
 
 @Component({})
 export default class Home extends Vue {
+
+  $refs!: {
+    modal1: BModal
+  };
+
   get appstate(): IAppState {
     return this.$store.state.App;
   }
